a3784ab48cb6191339cb3c60bfbf52161f78f5a2
sebastiangolijow <sebastian.golijow@gmail.com>
2021-04-07 16:35:12 +0200

    pruebaseba

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Searchbar} from './Components/Searchbar/Searchbar'
+import {Catalogo} from './Components/Catalogo/Catalogo'
+
+import { Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Route path='/' component={Searchbar}/>
+   <Route path='/' component={Catalogo}/>
+
     </div>
   );
 }
---
+++ client/src/Reducer/reducer.js
@@ -0,0 +1,24 @@
+const InitialState = {
+
+  };
+  
+  function Reducer(state = InitialState, action) {
+    switch (action.type) { 
+      case "":
+        return {};
+        
+      case "":
+        return {};
+  
+      case "": 
+        return {};
+  
+      case "":
+        return {};
+
+      default:
+        return state;
+    }
+  }
+  
+  export default Reducer;
