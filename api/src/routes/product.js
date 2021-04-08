762ff262f06209d310c9589bc41aa37dc77d900a
Flaviano Di Berardino <69056903+flavianodiberardino@users.noreply.github.com>
2021-04-08 11:36:03 -0300

    fixing get and post products routes and product model

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,17 +1,27 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const Sequelize = require('sequelize');
+const { Product, Category } = require('../db.js');
+const Op = Sequelize.Op;
 
 server.get('/', (req, res, next) => {
-	Product.findAll()
+	Product.findAll({ include: { model: Category }})
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
 });
 
-server.post('/newProduct', (req, res) => {
-	const { name, images, stock, price, model, brand, ranking, description, status, category } = req.body;
-	let description = JSON.parse(description)
+server.post('/newProduct', async (req, res) => {
+	let { name, images, stock, price, model, brand, ranking, description, status, categories } = req.body;
+
+	categories = categories.split(',')
+
+	categories = await Promise.all(categories.map(async category => await Category.findAll({
+		include: { model: Product },
+		where: {
+			name: { [Op.iLike]: `${category}` }
+		}
+	})));
 
 	try {
 		let products = await Product.create({
@@ -26,8 +36,7 @@ server.post('/newProduct', (req, res) => {
 			status
 		});
 
-		//let category = await findAll({});
-		await products.setCategories(category)
+		categories.map(async category => await products.setCategories(category))
 
 		res.send("New product")
 	} catch (e) {
