6140a76b59f7080251b2e86490d9957c1ba63413
SergioAriel <solis.sergioariel@gmail.com>
2021-04-09 00:26:17 -0300

    modificaciones, y correcciones, agregado de actions supabase

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Catalogue} from './Components/Catalogue/Catalogue'
-import {Product} from './Components/Product/Product'
-import {Nav} from './Components/Nav/Nav'
-import {Header} from './Components/Header/Header'
-import {Footer} from './Components/Footer/Footer'
-import { Route} from "react-router-dom";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Catalogue } from './Components/Catalogue/Catalogue'
+import { Product } from './Components/Product/Product'
+import { Nav } from './Components/Nav/Nav'
+// import {Header} from './Components/Header/Header'
+import { Footer } from './Components/Footer/Footer'
+import { Route, Router } from "react-router-dom"
+import { ProductCard } from './Components/ProductCard/ProductCard'
+import {Home} from './Components/Home/Home'
+
 
 function App() {
   return (
     <div className="App">
-   <Route path='/' component={Header}/>
-   <Route path='/' component={Nav}/>
-   <Route exact path='/product/:id' component={Product}/>
-   <Route path='/' component={Catalogue}/>
-   <Route path='/' component={Footer}/>
+      {/* <Route path='/' component={Header}/> */}
+      <Route path='/' component={Nav} />
+      <div className="">
+        <Route exact path='/Home' component={Home}/>
+        <Route exact path='/product/:id' component={Product} />
+        <Route exact path='/Catalogue' component={Catalogue} />
+        <Route path='/' component={Footer} />
+      </div>
     </div>
   );
 }
