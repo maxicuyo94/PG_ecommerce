cd20500a997c597a5ce44d722e87b9871d59145d
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2021-04-10 15:00:01 -0300

    Responsive Product

--- client/src/Components/Product/Slider/Slider.jsx
+++ client/src/Components/Product/Slider/Slider.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react'
-/* import Carousel, { Dots } from '@brainhubeu/react-carousel'
-import '@brainhubeu/react-carousel/lib/style.css' */
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import styles from './Slider.module.scss'
 const Slider = ({ images }) => {
-    //const [value, setValue] = useState(0);
-    //const onChange = value => { setValue(value) };
-
     return (
         <div className={styles.carousel}>
             <Carousel
+                //axis={'vertical'}
+                //centerMode={true}
+                //centerSlidePercentage={50}
+                showArrows={false}
                 showThumbs={true}
                 dynamicHeight={true}
                 infiniteLoop={true}
                 showIndicators={false}
                 showStatus={false}
+                useKeyboardArrows={true}
+                interval={1000}
+                //width={'80%'}
             >
                 {images?.map((slide, i) => {
                     return (
