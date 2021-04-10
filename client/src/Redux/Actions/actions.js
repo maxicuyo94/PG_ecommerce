a84f60ddc2af1714d609875df4800b272e4067ab
martuuu <martin.navarro.dev@gmail.com>
2021-04-09 21:50:17 -0300

    Form v1: agrega productos

--- client/src/Redux/Actions/actions.js
+++ client/src/Redux/Actions/actions.js
@@ -1,68 +1,68 @@
-import * as actionType from '../action_types/actionTypes'
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://zgycwtqkzgitgsycfdyk.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjE3NzMwOTg0LCJleHAiOjE5MzMzMDY5ODR9.8cmeNSjMvLmtlFtAwRjuR0VhXUhu5PX7174IBiXsU-E"
-const supabase = createClient(supabaseUrl, supabaseKey)
+import * as actionType from "../action_types/actionTypes";
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://zgycwtqkzgitgsycfdyk.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjE3NzMwOTg0LCJleHAiOjE5MzMzMDY5ODR9.8cmeNSjMvLmtlFtAwRjuR0VhXUhu5PX7174IBiXsU-E";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const Buscar =  (input) => {
+export const Buscar = (input) => {
   return async function (dispatch) {
-    const JSON =  await supabase
-    .from('product')
-    .select('*')
-    .ilike('name', `%${input}%`)
-    dispatch({type: actionType.SEARCH, payload: JSON.data})
-  }
-}
+    const JSON = await supabase
+      .from("product")
+      .select("*")
+      .ilike("name", `%${input}%`);
+    dispatch({ type: actionType.SEARCH, payload: JSON.data });
+  };
+};
 
-
-export const allProducts =  () => {
+export const allProducts = () => {
   return async function (dispatch) {
-    const JSON =  await supabase
-    .from('product')
-    .select("*")
-    console.log(JSON)
-    dispatch({type: actionType.SEARCH, payload: JSON.data})
-  }
-}
+    const JSON = await supabase.from("product").select("*");
+    console.log(JSON);
+    dispatch({ type: actionType.SEARCH, payload: JSON.data });
+  };
+};
 
-export const productDetail =  (input) => {
+export const productDetail = (input) => {
   return async function (dispatch) {
-    const JSON =  await supabase
-    .from('product')
-    .select("id")
-    dispatch({type: actionType.PRODUCT_DETAIL, payload: JSON.data})
-  }
-}
+    const JSON = await supabase.from("product").select("id");
+    dispatch({ type: actionType.PRODUCT_DETAIL, payload: JSON.data });
+  };
+};
 
 export const getCategories = () => {
   return async function (dispatch) {
-    const JSON = await supabase
-    .from('categories')
-    .select('*')
-    dispatch({type: actionType.GET_CATEGORIES, payload: JSON.data})
-  }
-}
+    const JSON = await supabase.from("categories").select("*");
+    dispatch({ type: actionType.GET_CATEGORIES, payload: JSON.data });
+  };
+};
 
 export const getProductsByCategories = (id) => {
   return async function (dispatch) {
-    const JSON = await supabase
-    .from('product_categories')
-    .select(`${id}`)
-    console.log(JSON)
+    const JSON = await supabase.from("product_categories").select(`${id}`);
+    console.log(JSON);
     // dispatch({type: actionType.GET_CATEGORIES, payload: JSON.data})
-  }
-}
-
+  };
+};
 
 export const postProduct = (product) => {
   return async function (dispatch) {
-    const { data, error } = await supabase
-    .from('product')
-    .insert([
-      { name: `${product.name}`, images: `${product.images}`, stock: `${product.stock}`, price: `${product.price}`, model: `${product.model}`,
-      brand: `${product.brand}`, description: `${product.description}`
-     },
-    ])
-    dispatch({type: actionType.POST_PRODUCT})
-  }
-}
+    const resp = await supabase
+      .from("product")
+      .insert([
+        {
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          // images: product.images,
+          brand: product.brand,
+          stock: product.stock,
+          model: product.model,
+          ranking: product.ranking,
+          storage: product.storage,
+          status: product.status,
+        },
+      ]);
+    dispatch({ type: actionType.POST_PRODUCT });
+  };
+};
