dbba89a5e7056cc70df82e9fc91734fd761b0565
vcoronado <vcoronadoasm@gmail.com>
2021-04-12 04:01:26 -0300

    New component

--- client/src/Components/ProductNotFound/ProductNotFound.jsx
+++ client/src/Components/ProductNotFound/ProductNotFound.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Style from "./ProductNotFound.module.scss";
+
 const image = `image.png`;
 
 const ProductNotFound = () => {
   return (
-    <div className="notFound">
-      <h1>Nothing here, nothing there...</h1>
-      <h3>Try again in a few days!</h3>
-      <img className="imag" src={image} alt="Product not found"></img>
+    <div className={Style.container}>
+      <div className="notFound">
+        <h1>Nothing here, nothing there...</h1>
+        <h3>Try again in a few days!</h3>
+        <img className="imag" src={image} alt="Product not found"></img>
+      </div>
     </div>
   );
 };
