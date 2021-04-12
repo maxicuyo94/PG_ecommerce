e8080ed80b8de039e847e7690f19544e62d46358
vcoronado <vcoronadoasm@gmail.com>
2021-04-12 02:49:06 -0300

    Formatting code api's folder

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -1,26 +1,23 @@
-const { DataTypes } = require('sequelize');
-
-// Exportamos una funcion que define el modelo y le injectamos la conexion a Sequelize
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    // Definimos el modelo
-    sequelize.define('category', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        active: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        }
-    })
+  sequelize.define("category", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
 };
