8a3eee326902921a1ac495812d37ea4002255dda
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2021-04-14 22:53:40 -0300

    MiniShop Created

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { SearchBar } from "../Searchbar/Searchbar";
+import { Link, NavLink } from "react-router-dom";
+import { SearchBar } from "../SearchBar/SearchBar";
 import { Header } from "./Header/Header";
 import { useTranslation } from "react-i18next";
 import style from "./nav.module.scss";
-
+import MiniShop from './MiniShop/MiniShop'
 export function Nav() {
   const [t, i18n] = useTranslation("global");
 
@@ -12,6 +12,11 @@ export function Nav() {
     <div className={style.container}>
       <Header />
       <div className={style.contents}>
+        <div className={style.logo}>
+          <NavLink to={'/landing'}>
+            <img src={'https://res.cloudinary.com/techstore/image/upload/v1618082875/edobvt8ghwyblnagtkoj.png'} />
+          </NavLink>
+        </div>
         <ul>
           <li>
             <Link to="/">
@@ -28,9 +33,6 @@ export function Nav() {
               <text>{t("navLink3.linkThree")}</text>
             </Link>
           </li>
-          {/* <li>
-              <SearchBar />
-            </li> */}
           <li>
             <Link to="/login">
               <text>{t("navLink4.linkFour")}</text>
@@ -40,6 +42,9 @@ export function Nav() {
             <Link to="/register">
               <text>{t("navLink5.linkFive")}</text>
             </Link>
+          </li>
+          <li>
+            <MiniShop />
           </li>
           <li>
             <button onClick={() => i18n.changeLanguage("es")}>ES</button>
