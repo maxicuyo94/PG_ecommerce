094b6de127a4a70fbe0e566a3c6a1707e793aaf5
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2021-04-15 23:27:40 -0300

    ArreglosGenerales

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { SearchBar } from "../SearchBar/SearchBar";
+//import { SearchBar } from "../SearchBar/SearchBar";
 import { Header } from "./Header/Header";
 import { useTranslation } from "react-i18next";
 import style from "./nav.module.scss";
 import MiniShop from './MiniShop/MiniShop'
+import BtnLang from './BtnLang/BtnLang'
 export function Nav() {
   const [t, i18n] = useTranslation("global");
 
   return (
     <div className={style.container}>
-        <Header />
-        <div className={style.contents}>
-          <div className={style.logo}>
+      <Header />
+      <div className={style.contents}>
+        <div className={style.logo}>
           <NavLink to={'/landing'}>
             <img src={'https://res.cloudinary.com/techstore/image/upload/v1618082875/edobvt8ghwyblnagtkoj.png'} />
           </NavLink>
         </div>
-          <ul>
-            <li>
-              <Link to="/">
-                <text>Home</text>
-              </Link>
-            </li>
-            <li>
-              <Link to='/catalogue'>
-                <text>
-                  Catalogue
+        <ul>
+          <li>
+            <Link to="/">
+              <text>Home</text>
+            </Link>
+          </li>
+          <li>
+            <Link to='/catalogue'>
+              <text>
+                Catalogue
                 </text>
-              </Link>
-            </li>
-            <li>
-              <Link to='/controlpanel'>
-                <text>
-                  Control Panel
+            </Link>
+          </li>
+          <li>
+            <Link to='/controlpanel'>
+              <text>
+                Control Panel
                 </text>
-              </Link>
-            </li>
-            <li>
-              <MiniShop />
-            </li>
+            </Link>
+          </li>
           <li>
             <Link to="/login">
               <text>{t("navLink4.linkFour")}</text>
@@ -51,10 +49,14 @@ export function Nav() {
             </Link>
           </li>
           <li>
-            <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+            <MiniShop />
           </li>
           <li>
-            <button onClick={() => i18n.changeLanguage("en")}>EN</button>
+            <BtnLang />
+            {/* <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+          </li>
+          <li>
+            <button onClick={() => i18n.changeLanguage("en")}>EN</button> */}
           </li>
         </ul>
       </div>
--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -2,60 +2,46 @@ import React from 'react';
 import Style from './productcard.module.scss'
 import greyheart from './greyheart.png'
 import sCart from './shopping-cart.svg'
-// import revs from './grafico-de-barras.png'
+import revs from './grafico-de-barras.png'
 import { Link } from 'react-router-dom'
 import swal from 'sweetalert';
-import { useDispatch } from 'react-redux';
-import { addItemCart } from '../../Redux/Actions/cartActions';
-
-
-
-
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 export function ProductCard(props) {
-	const dispatch = useDispatch()
-	const handleAddCart = (props) => {
-		const newItem = {
-			id: props.id,
-			images: props.images,
-			title: props.name,
-			quantity: 1,
-			price: props.price,
-			stock: props.stock
-		}
-		dispatch(addItemCart(newItem))
-		swal("Done!", "Added to cart", "success");
-	}
 
 	return (
 		<div className={Style.container}>
-			<Link to={`/product/${props.id}`}>
-				<div className={Style.favorite}>
-					<img src={greyheart} />
-					{/* <img src={revs}/> */}
-				</div>
-				<img src={props.images} className={Style.img} alt={props.name} />
-				<div className={Style.review}>
-					<label>★</label>
-					<label>★</label>
-					<label>★</label>
-					<label>★</label>
-					<label>★</label>
-					<p>Reviews (4)</p>
-				</div>
-				<div className={Style.name}>
-					<p>{props.name}</p>
-				</div>
-				<div className={Style.price}>
-					<p><strong>US$ {props.price}</strong></p>
-				</div>
-				<div>
-					{props.stock > 0 ? <button img src={sCart} onClick={() => handleAddCart(props)} >
-						Add to Cart
-                        </button> : <button img src={sCart} onClick={() => { swal("Sorry!", "Come back in a few days", "error"); }} >
-						Sold out
-                        </button>}
-				</div>
-			</Link>
-		</div>
+			{/* <div className={Style.favorite}>
+				<span><FavoriteIcon /></span>
+			</div> */}
+			<div className={Style.image} >
+				<img src={props.images} alt='.' />
+			</div>
+			{/* <div className={Style.review}>
+				<label>★</label>
+				<label>★</label>
+				<label>★</label>
+				<label>★</label>
+				<label>★</label>
+				<p>Reviews (4)</p>
+			</div> */}
+			<div className={Style.name}>
+				<span>{props.name}</span>
+			</div>
+			<div className={Style.price}>
+				<span>US$<b>{props.price}</b></span>
+			</div>
+
+			{props.stock > 0 ?
+				<button
+					onClick={() => { swal("Done!", "Added to cart", "success"); }} >
+					Add to Cart
+                </button>
+				:
+				<button
+					onClick={() => { swal("Sorry!", "Come back in a few days", "error"); }} >
+					Sold out
+                </button>}
+		</div >
 	)
 }
