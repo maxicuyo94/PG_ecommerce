85fcacceb024b426561b22bec58feade72097394
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2021-04-18 22:37:41 -0300

    Muchos arreglos

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Style from './productcard.module.scss'
-import greyheart from './greyheart.png'
-import sCart from './shopping-cart.svg'
-import revs from './grafico-de-barras.png'
-import { Link, NavLink } from 'react-router-dom'
+
+import { NavLink } from 'react-router-dom'
 import swal from 'sweetalert';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import BlockIcon from '@material-ui/icons/Block';
 import { addItemCart } from '../../Redux/Cart/cartActions';
@@ -28,19 +28,20 @@ export function ProductCard(props) {
 	}
 	return (
 		<div className={props.stock ? Style.container : Style.off} >
-			<div className={Style.stock}>
-				{props.stock > 0 ?
-					<span className={Style.on}> On Stock! <CheckCircleIcon fontSize="small" /></span>
-					:
-					<span className={Style.out}> Sold Out! <BlockIcon fontSize="small" /></span>
-				}
-			</div>
-			<div className={Style.image} >
-				<NavLink to={`/product/${props.id}`}>
-					<img src={props.images} alt='.' />
-				</NavLink>
-			</div>
-			{/* <div className={Style.review}>
+			<div className={Style.card}>
+				<div className={Style.stock}>
+					{props.stock > 0 ?
+						<span className={Style.on}> On Stock! <CheckCircleIcon fontSize="small" /></span>
+						:
+						<span className={Style.out}> Sold Out! <BlockIcon fontSize="small" /></span>
+					}
+				</div>
+				<div className={Style.image} >
+					<NavLink to={`/product/${props.id}`}>
+						<img src={props.images} alt='.' />
+					</NavLink>
+				</div>
+				{/* <div className={Style.review}>
 				<label>★</label>
 				<label>★</label>
 				<label>★</label>
@@ -48,26 +49,28 @@ export function ProductCard(props) {
 				<label>★</label>
 				<p>Reviews (4)</p>
 			</div> */}
-			<div className={Style.name}>
-				<NavLink to={`/product/${props.id}`}>
-					{props.title?.split(" ").slice(0, 3).join(" ")}
-				</NavLink>
-			</div>
-			<div className={Style.price}>
-				<span>US$<b>{props.price}</b></span>
+				<div className={Style.name}>
+					<NavLink to={`/product/${props.id}`}>
+						{props.title?.split(" ").slice(0, 3).join(" ")}
+					</NavLink>
+				</div>
+				<div className={Style.price}>
+					<span>US$<b>{props.price}</b></span>
+				</div>
+
 			</div>
 
 			{props.stock > 0 ?
 				<button className={Style.btnOn}
 					onClick={() => { handleAddToCart(props) }} >
-					Add to Cart
-                </button>
+					<span> Add to Cart!</span> <AddShoppingCartIcon />
+				</button>
 				:
-				<button className={Style.btnOut}
+				<button className={Style.btnOff}
 					onClick={() => { swal("Sorry!", "Come back in a few days", "error"); }} >
-					Sold out
-                </button>
+					<span> Sold Out! </span> <RemoveShoppingCartIcon />
+				</button>
 			}
-		</div >
+		</div>
 	)
 }
