8520e5c384c24d5a4a5708f807566934094d3dbf
Flaviano Di Berardino <69056903+flavianodiberardino@users.noreply.github.com>
2021-04-27 14:06:34 -0300

    whatsapp icon

--- client/src/Components/Layout/Layout.jsx
+++ client/src/Components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Nav } from "../Nav/Nav";
 import { Footer } from "../Footer/Footer";
 import styles from "./layout.module.scss";
+import Whatsapp from "../Whatsapp/Whatsapp";
 
 function Layout({ children, priority, dark }) {
   return (
     <div className={dark ? styles.containerDark : styles.container}>
+    <Whatsapp/>
       <main>
         <Nav priority={priority} dark={dark}/>
         {children}
