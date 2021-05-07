9420eedc8e04c8bb47d536a01f55eb71b932eb4d
SergioAriel <solis.sergioariel@gmail.com>
2021-05-07 13:25:07 -0300

    intento numero 7000

--- client/src/Components/Points/Points.jsx
+++ client/src/Components/Points/Points.jsx
@@ -2,41 +2,11 @@ import React, { useEffect, useRef, useState } from 'react'
 import styles from './Points.module.scss'
 
 export function Points() {
-    // const rectangulo = [
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    //     [false, false, false, false],
-    // ]
-    
-    // useEffect(()=>{
-    //     if (!rectangulo.all.all){
-    //         rectangulo[0][Math.random()]
-    //     } si esta vacio
-    //     else{
-    //       setTimeout(() => {
-    //           rectangugulo[fila1][columna3]
-    //           si fila+1 extiste rectangugulo[fila1+1][columna3]
-    //       }, 100
-    //     }
-  
-    // }
-    // ),[rectangulo])
-    
-    // rectangulo.map()
+
 
     return (
         <div className={styles.container}>
             <div className={styles.contents}>
-                {/* <div className={styles.cascade} ref={cascade}>
-                    {cascadeChildrens && cascadeChildrens[0]}
-                </div> */}
                 <h1 className={styles.tag}>TechStore Points</h1>
                 <div className={styles.border}>
                     <section className={styles.section}>
